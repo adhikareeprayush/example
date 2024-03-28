eacfbe63ce8ca2be57d6f2fabe989b563693bd5e
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-03-28 21:42:08 +0545

    Project Created

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h2>Hello From The About Page</h2>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <h2>Hello From The Contact Page</h2>
+</x-layout>
