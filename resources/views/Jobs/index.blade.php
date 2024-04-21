a6b849fb8c728ae80650eb65c29b446a7da99a44
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-04-21 10:33:24 +0545

    Routes and Controllers

--- resources/views/Jobs/index.blade.php
+++ resources/views/Jobs/index.blade.php
@@ -4,18 +4,18 @@
     </x-slot:heading>
 
     <div class="space-y-4">
-    @foreach ($jobs as $job)
-    <a href="/job/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-        <div class="font-bold text-blue-500 text-sm">{{$job->employer->company_name}}</div>
-        <div>
-            <strong>{{$job['title']}} </strong>: Pays ${{$job['salary']}} USD.
-        </div>
-    </a>
-    @endforeach
-    </div> 
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->company_name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }} </strong>: Pays ${{ $job['salary'] }} USD.
+                </div>
+            </a>
+        @endforeach
+    </div>
 
     <div class="mt-2">
-        {{$jobs->links()}}
+        {{ $jobs->links() }}
     </div>
 
 
--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -3,9 +3,14 @@
         Jobs Page
     </x-slot:heading>
 
-    <h2 class="font-bold text-black-100">{{$job['title']}}</h2>
+    <h2 class="font-bold text-black-100">{{ $job->title }}</h2>
     <p>
-        This job pays {{$job['salary']}} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
+
+
+    <div class="mt-6 flex gap-3">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </div>
 
 </x-layout>
